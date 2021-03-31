9ae28002e181dfd038a3addc023016d304e0ce9b
JulioHC00 <81090346+JulioHC00@users.noreply.github.com>
2021-04-01 00:12:11 +0100

    Update cooling.py

--- cooling.py
+++ cooling.py
@@ -14,51 +14,65 @@ halfsolardens = 1.9677e9
 m_u = 1.6605390666e-27
 million_years = 1e6*365*24*3600
 
-def calculate(M, T_o, R, Y_e_core, X, Y, Z, solver = 'RK23', r_tol = 1e-3, a_tol = 1e-6, graphs = False, t_max = 100):
+def calculate(M, T_o, R, Y_e_core, mu_core, X, Y, Z, solver = 'RK23', r_tol = 1e-3, a_tol = 1e-6, graphs = False, t_max = 100, alpha = 0, storing_times = None):
     kappa_o = 4.34e23*Z*(1+X)
-    mu_e = 1/Y_e_core
-    mu = 2/(1+3*X+0.5*Y)
-    
+    mu_envelope = 2/(1+3*X+0.5*Y)
+    mu_e_core = 1/Y_e_core
+    mu_ion_core = ((1/mu_core)-(1/mu_e_core))**(-1)
     Cv = (3/2)*sc.k
-    
-    def equations(t, T):
-        rho_c = 6e-6*mu_e*T**(3/2)
-        L = (32/(3*8.5))*sc.sigma*(4*sc.pi*sc.G*M/kappa_o)*mu*m_u/(sc.k)*T**(6.5)/(rho_c**2)
+    if alpha == 0:
+        def equations(t, T):
+            rho_c = 6e-6*mu_e_core*T**(3/2)
+            L = (32/(3*8.5))*sc.sigma*(4*sc.pi*sc.G*M/kappa_o)*mu_envelope*m_u/(sc.k)*T**(6.5)/(rho_c**2)
+
+            dTdt = -1000*million_years*L*mu_ion_core*m_u/(Cv*M)
+            return dTdt
+    else:
+        def equations(t, T):
+            rho_c = 6e-6*mu_e_core*T**(3/2)
+            L = (32/(3*8.5))*sc.sigma*(4*sc.pi*sc.G*M/kappa_o)*mu_envelope*m_u/(sc.k)*T**(6.5)/(rho_c**2)
+
+            dTdt = -1000*million_years*(L+alpha*T)*mu_ion_core*m_u/(Cv*M)
+            return dTdt
         
-        dTdt = -1000*million_years*L*mu*m_u/(Cv*M)
-        return dTdt
     
-    cool = solve_ivp(equations, [0,t_max], [T_o], method = solver, rtol = r_tol, atol = a_tol)
+    cool = solve_ivp(equations, [0,t_max], [T_o], method = solver, rtol = r_tol, atol = a_tol, t_eval = storing_times)
     
     class evolution:
         time = cool.t*million_years*1000
         core_temperature = cool.y[0] 
-        luminosity = (32/(3*8.5))*sc.sigma*(4*sc.pi*sc.G*M/kappa_o)*mu*m_u/(sc.k)*core_temperature**(3.5)/((6e-6*mu_e)**2)
+        luminosity = (32/(3*8.5))*sc.sigma*(4*sc.pi*sc.G*M/kappa_o)*mu_envelope*m_u/(sc.k)*core_temperature**(3.5)/((6e-6*mu_e_core)**2)
         surface_temperature = (luminosity/(4*sc.pi*R**2*sc.sigma))**(1/4)
         
     if graphs:
-        fig, ax = plt.subplots(3,1, dpi =200, figsize = (5,30))
-        ax[0].plot(evolution.time/(1000*million_years), evolution.core_temperature)
-        ax[0].set_xlabel('Billion years')
-        ax[0].set_ylabel('Core temperature [K]')
-        ax[0].grid()
+        fig, ax = plt.subplots(2,2, dpi =200, figsize = (20,20))
+        ax[0,0].plot(evolution.time/(1000*million_years), evolution.core_temperature)
+        ax[0,0].set_xlabel('Billion years')
+        ax[0,0].set_ylabel('Core temperature [K]')
+        ax[0,0].grid()
         
-        ax[1].plot(evolution.time/(1000*million_years), evolution.surface_temperature)
-        ax[1].set_xlabel('Billion years')
-        ax[1].set_ylabel('Surface temperature [K]')
-        ax[1].grid()
+        ax[0,1].plot(evolution.time/(1000*million_years), evolution.surface_temperature)
+        ax[0,1].set_xlabel('Billion years')
+        ax[0,1].set_ylabel('Surface temperature [K]')
+        ax[0,1].grid()
         
-        ax[2].plot(evolution.time/(1000*million_years), evolution.luminosity/L_sun)
-        ax[2].set_xlabel('Billion years')
-        ax[2].set_ylabel('Solar luminosities')
-        ax[2].grid()
+        ax[1,0].plot(evolution.time/(1000*million_years), evolution.luminosity/L_sun)
+        ax[1,0].set_xlabel('Billion years')
+        ax[1,0].set_ylabel('Solar luminosities')
+        ax[1,0].grid()
         
     
     return evolution
 
-def full_calculate(rho_core, T_core, Y_e_core, X, Y, Z, solver = 'RK23', r_tol = 1e-3, a_tol = 1e-6, graphs = False, t_max = 100):
+def full_calculate(rho_core, T_core,Y_e_core, C, X, Y, Z, solver = 'RK23', r_tol = 1e-3, a_tol = 1e-6, graphs = False, t_max = 100, full_output = False, storing_times = None):
+    
+    mu_core = ((1/48)*C+9/16)**(-1)
 
     env, cor = envelope.solve(rho_core,T_core, Y_e_core, X, Y, Z, solver = solver, r_tol_core = r_tol, r_tol_envelope = r_tol, a_tol_core = a_tol, a_tol_envelope = a_tol, message = False)
     
-    evolution = calculate(cor.mass[-1], cor.temperature[-1], env.radius[-1], Y_e_core, X, Y, Z, graphs = graphs, solver = solver, t_max = t_max)
-    return evolution
+    evolution = calculate(cor.mass[-1], cor.temperature[-1], env.radius[-1], Y_e_core, mu_core, X, Y, Z, graphs = graphs, solver = solver, t_max = t_max, storing_times = storing_times, r_tol = r_tol, a_tol = a_tol)
+    
+    if full_output:
+        return evolution, env, cor
+    elif ~full_output:
+        return evolution
