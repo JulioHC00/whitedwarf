23989f5360c1926fa8994375de41732114038eda
JulioHC00 <81090346+JulioHC00@users.noreply.github.com>
2021-04-02 23:03:46 +0100

    Add files via upload

--- cooling.py
+++ cooling.py
@@ -11,105 +11,159 @@ import scipy.constants as sc
 
 rho_core_sun = 1.62e5
 rho_mean_sun = 1406.3134
-R_sun = 6.9634*1e8
-M_sun = 2*1e30
+R_sun = 6.9634 * 1e8
+M_sun = 2 * 1e30
 L_sun = 3.828e26
 halfsolardens = 1.9677e9
 m_u = 1.6605390666e-27
-million_years = 1e6*365*24*3600
+million_years = 1e6 * 365 * 24 * 3600
 
-def calculate(M, T_o, R, Y_e_core, mu_core, X, Y, Z, core_class, C, solver = 'RK23', r_tol = 1e-3, a_tol = 1e-6, graphs = False, t_max = 100, alpha = 0, beta =0, storing_times = None, crys = True):
+
+def calculate(
+        M,
+        T_o,
+        R,
+        Y_e_core,
+        mu_core,
+        X,
+        Y,
+        Z,
+        core_class,
+        C,
+        solver='RK23',
+        r_tol=1e-3,
+        a_tol=1e-6,
+        graphs=False,
+        t_max=100,
+        alpha=0,
+        beta=0,
+        storing_times=None,
+        crys=True):
     '''
     AUXILIARY FOR FULL_CALCULATE
     '''
     cor = core_class
-    kappa_o = 4.34e23*Z*(1+X)
-    mu_envelope = 2/(1+3*X+0.5*Y)
-    mu_e_core = 1/Y_e_core
-    mu_ion_core = ((1/mu_core)-(1/mu_e_core))**(-1)
-    Cv = (3/2)*sc.k
-    Cv_crystal = 3*sc.k
+    kappa_o = 4.34e23 * Z * (1 + X)
+    mu_envelope = 2 / (1 + 3 * X + 0.5 * Y)
+    mu_e_core = 1 / Y_e_core
+    mu_ion_core = ((1 / mu_core) - (1 / mu_e_core))**(-1)
+    Cv = (3 / 2) * sc.k
+    Cv_crystal = 3 * sc.k
     mass = np.flip(cor.mass)
     if alpha == 0:
         def equations(t, T):
-            
-            for i, rho in enumerate(np.flip(cor.density)):
-                gamma = (1/(4*sc.pi*sc.epsilon_0))*(((6*C+8*(1-C))*sc.e)**2/(sc.k*T))*(4*sc.pi*rho/(3*mu_ion_core*m_u))**(1/3)
-                
-                if gamma >= 171 and crys:
-                    mass_energy = ((mass[0]-mass[i])*Cv+(mass[i])*Cv_crystal)/(mu_ion_core*m_u)
-                    break
-                elif i+1 == len(cor.density):
-                    mass_energy = M*Cv/(mu_ion_core*m_u)
-            
-            rho_c = 6e-6*mu_e_core*T**(3/2)
-            L = (32/(3*8.5))*sc.sigma*(4*sc.pi*sc.G*M/kappa_o)*mu_envelope*m_u/(sc.k)*T**(6.5)/(rho_c**2)
 
-            dTdt = -1000*million_years*L/mass_energy
+            for i, rho in enumerate(np.flip(cor.density)):
+                gamma = (1 / (4 * sc.pi * sc.epsilon_0)) * (((6 * C + 8 * (1 - C)) * sc.e)
+                                                            ** 2 / (sc.k * T)) * (4 * sc.pi * rho / (3 * mu_ion_core * m_u))**(1 / 3)
+
+                if gamma >= 171 and crys:
+                    mass_energy = (
+                        (mass[0] - mass[i]) * Cv + (mass[i]) * Cv_crystal) / (mu_ion_core * m_u)
+                    break
+                elif i + 1 == len(cor.density):
+                    mass_energy = M * Cv / (mu_ion_core * m_u)
+
+            rho_c = 6e-6 * mu_e_core * T**(3 / 2)
+            L = (32 / (3 * 8.5)) * sc.sigma * (4 * sc.pi * sc.G * M / \
+                 kappa_o) * mu_envelope * m_u / (sc.k) * T**(6.5) / (rho_c**2)
+
+            dTdt = -1000 * million_years * L / mass_energy
             return dTdt
     else:
         def equations(t, T):
             for i, rho in enumerate(np.flip(cor.density)):
-                gamma = (1/(4*sc.pi*sc.epsilon_0))*(((6*C+8*(1-C))*sc.e)**2/(sc.k*T))*(4*sc.pi*rho/(3*mu_ion_core*m_u))**(1/3)
-                
-                if gamma >= 171 and crys:
-                    mass_energy = ((mass[0]-mass[i])*Cv+(mass[i])*Cv_crystal)/(mu_ion_core*m_u)
-                    break
-                elif i+1 == len(cor.density):
-                    mass_energy = M*Cv/(mu_ion_core*m_u)
-            
-            rho_c = 6e-6*mu_e_core*T**(3/2)
-            L = (32/(3*8.5))*sc.sigma*(4*sc.pi*sc.G*M/kappa_o)*mu_envelope*m_u/(sc.k)*T**(6.5)/(rho_c**2)
+                gamma = (1 / (4 * sc.pi * sc.epsilon_0)) * (((6 * C + 8 * (1 - C)) * sc.e)
+                                                            ** 2 / (sc.k * T)) * (4 * sc.pi * rho / (3 * mu_ion_core * m_u))**(1 / 3)
 
-            dTdt = -1000*million_years*(L+alpha*T**beta)/mass_energy
+                if gamma >= 171 and crys:
+                    mass_energy = (
+                        (mass[0] - mass[i]) * Cv + (mass[i]) * Cv_crystal) / (mu_ion_core * m_u)
+                    break
+                elif i + 1 == len(cor.density):
+                    mass_energy = M * Cv / (mu_ion_core * m_u)
+
+            rho_c = 6e-6 * mu_e_core * T**(3 / 2)
+            L = (32 / (3 * 8.5)) * sc.sigma * (4 * sc.pi * sc.G * M / \
+                 kappa_o) * mu_envelope * m_u / (sc.k) * T**(6.5) / (rho_c**2)
+
+            dTdt = -1000 * million_years * (L + alpha * T**beta) / mass_energy
             return dTdt
-        
-    
-    cool = solve_ivp(equations, [0,t_max], [T_o], method = solver, rtol = r_tol, atol = a_tol, t_eval = storing_times)
-    
+
+    cool = solve_ivp(equations,
+                     [0,
+                      t_max],
+                     [T_o],
+                     method=solver,
+                     rtol=r_tol,
+                     atol=a_tol,
+                     t_eval=storing_times)
+
     class evolution:
-        time = cool.t*million_years*1000
-        core_temperature = cool.y[0] 
-        luminosity = (32/(3*8.5))*sc.sigma*(4*sc.pi*sc.G*M/kappa_o)*mu_envelope*m_u/(sc.k)*core_temperature**(3.5)/((6e-6*mu_e_core)**2)
-        surface_temperature = (luminosity/(4*sc.pi*R**2*sc.sigma))**(1/4)
-        
+        time = cool.t * million_years * 1000
+        core_temperature = cool.y[0]
+        luminosity = (32 / (3 * 8.5)) * sc.sigma * (4 * sc.pi * sc.G * M / kappa_o) * \
+            mu_envelope * m_u / (sc.k) * core_temperature**(3.5) / ((6e-6 * mu_e_core)**2)
+        surface_temperature = (
+            luminosity / (4 * sc.pi * R**2 * sc.sigma))**(1 / 4)
+
     if graphs:
-        fig, ax = plt.subplots(2,2, dpi =200, figsize = (20,20))
-        ax[0,0].plot(evolution.time/(1000*million_years), evolution.core_temperature)
-        ax[0,0].set_xlabel('Billion years')
-        ax[0,0].set_ylabel('Core temperature [K]')
-        ax[0,0].grid()
-        
-        ax[0,1].plot(evolution.time/(1000*million_years), evolution.surface_temperature)
-        ax[0,1].set_xlabel('Billion years')
-        ax[0,1].set_ylabel('Surface temperature [K]')
-        ax[0,1].grid()
-        
-        ax[1,0].plot(evolution.time/(1000*million_years), evolution.luminosity/L_sun)
-        ax[1,0].set_xlabel('Billion years')
-        ax[1,0].set_ylabel('Solar luminosities')
-        ax[1,0].grid()
-        
-    
+        fig, ax = plt.subplots(2, 2, dpi=200, figsize=(20, 20))
+        ax[0, 0].plot(evolution.time / (1000 * million_years),
+                      evolution.core_temperature)
+        ax[0, 0].set_xlabel('Billion years')
+        ax[0, 0].set_ylabel('Core temperature [K]')
+        ax[0, 0].grid()
+
+        ax[0, 1].plot(evolution.time / (1000 * million_years),
+                      evolution.surface_temperature)
+        ax[0, 1].set_xlabel('Billion years')
+        ax[0, 1].set_ylabel('Surface temperature [K]')
+        ax[0, 1].grid()
+
+        ax[1, 0].plot(evolution.time / (1000 * million_years),
+                      evolution.luminosity / L_sun)
+        ax[1, 0].set_xlabel('Billion years')
+        ax[1, 0].set_ylabel('Solar luminosities')
+        ax[1, 0].grid()
+
     return evolution
 
-def full_calculate(rho_core, T_core,Y_e_core, C, X, Y, Z, solver = 'RK23', r_tol = 1e-3, a_tol = 1e-6, graphs = False, t_max = 14, full_output = False, storing_times = None, alpha = 0, beta = 0, crys = True):
+
+def full_calculate(
+        rho_core,
+        T_core,
+        Y_e_core,
+        C,
+        X,
+        Y,
+        Z,
+        solver='RK23',
+        r_tol=1e-3,
+        a_tol=1e-6,
+        graphs=False,
+        t_max=14,
+        full_output=False,
+        storing_times=None,
+        alpha=0,
+        beta=0,
+        crys=True):
     '''
     Description
     -----------
     CALCULATES THE COOLING TRACK OF A WHITE DWARF RELYING ON THE CORE
     AND ENVELOPE MODULES TO FIRST SET THE STRUCTURE OF THE STAR.
-    
+
     C/O CORE.
-    
+
     INCLUDES THE CRYSTALLIZATION OF THE CORE WITH THE CRYSTALLIZATION
     FRONT ADVANCING TOWARDS THE ENVELOPE.
-    
+
     DOES NOT INCLUDE DEBYE COOLING.
-    
+
     Parameters
     ----------
-    
+
     rho_core: float
         Density of the core at r=0 in kg/m^3.
     T_core: float
@@ -159,29 +213,48 @@ def full_calculate(rho_core, T_core,Y_e_core, C, X, Y, Z, solver = 'RK23', r_tol
         For test purposes, whether to include crystallization
         in the cooling or not.
         Default is True.
-    
+
     Returns
     -------
     evolution: class
         Contain the values for different properties of the star:
-        
+
         evolution.time                | Time in seconds
         evolution.luminosity          | Evolution of the star luminosity in W
         evolution.core_temperature    | Evolution of the core temperature
         evolution.surface_temperature | Evolution of the surface temperature
-        
-    Additionally if full_return it will also return the core and envelope classes from the envelope module as
-    
-    evolution, envelope, core
-    
-    '''
-    
-    mu_core = ((1/48)*C+9/16)**(-1)
 
-    env, cor = envelope.solve(rho_core,T_core, Y_e_core, X, Y, Z, solver = solver, r_tol_core = r_tol, r_tol_envelope = r_tol, a_tol_core = a_tol, a_tol_envelope = a_tol, message = False)
-    
-    evolution = calculate(cor.mass[-1], cor.temperature[-1], env.radius[-1], Y_e_core, mu_core, X, Y, Z, cor, C, graphs = graphs, solver = solver, t_max = t_max, storing_times = storing_times, r_tol = r_tol, a_tol = a_tol, alpha=alpha, beta = beta, crys = crys)
-    
+    Additionally if full_return it will also return the core and envelope classes from the envelope module as
+
+    evolution, envelope, core
+
+    '''
+
+    mu_core = ((1 / 48) * C + 9 / 16)**(-1)
+
+    env, cor = envelope.solve(rho_core, T_core, Y_e_core, X, Y, Z, solver=solver, r_tol_core=r_tol,
+                              r_tol_envelope=r_tol, a_tol_core=a_tol, a_tol_envelope=a_tol, message=False)
+
+    evolution = calculate(cor.mass[-1],
+                          cor.temperature[-1],
+                          env.radius[-1],
+                          Y_e_core,
+                          mu_core,
+                          X,
+                          Y,
+                          Z,
+                          cor,
+                          C,
+                          graphs=graphs,
+                          solver=solver,
+                          t_max=t_max,
+                          storing_times=storing_times,
+                          r_tol=r_tol,
+                          a_tol=a_tol,
+                          alpha=alpha,
+                          beta=beta,
+                          crys=crys)
+
     if full_output:
         return evolution, env, cor
     elif ~full_output:
--- core.py
+++ core.py
@@ -12,34 +12,51 @@ import scipy.constants as sc
 
 m_u = 1.6605390666e-27
 
-def solve(rho, T_c, Y_e, rho_r = -1, graphs = False, R_r = 6.9634e8, r_o = 0.0001, x_max = -1, density_event = False, messages = False, min_density = 1e-15, solver = "RK23", r_tol = 1e-3, a_tol = 1e-6, X=-1, Y=-1, Z=-1):
 
+def solve(
+        rho,
+        T_c,
+        Y_e,
+        rho_r=-1,
+        graphs=False,
+        R_r=6.9634e8,
+        r_o=0.0001,
+        x_max=-1,
+        density_event=False,
+        messages=False,
+        min_density=1e-15,
+        solver="RK23",
+        r_tol=1e-3,
+        a_tol=1e-6,
+        X=-1,
+        Y=-1,
+        Z=-1):
     '''
-    
+
     Description
     -----------
     Solves the core of a White Dwarf using scipy.integrate.solve_ivp
-    
+
     Stops when temperature of the core equals fermi temperature.
     Works under the following assumptions:
-    
+
     - Isothermal core
     - Constant element distribution with free electrons per nucleon equal to Y_e
     - Full degeneracy all through the core
     - Only source of pressure is electron degeneracy pressure
     - Classical fermi momentum sqrt(2*m_e*E_fermi). The calculation doesn't work in the high relativistic regime, it deviates.
-    
+
     Definition of reduced variables is:
-    
+
     reduced density = density/rho_r
     reduced radius = r/R_r
     reduced mass = M/(4/3*pi*R_r^3*rho_r)
     reduced temperature = T/T_c
     reduced pressure = P/P_c [Where P_c is the degenerate electron pressure at rho_r density]
-    
+
     Parameters
     ----------
-    
+
     param rho: float, central density of the star in kg/m^3.
     T_c: float
         core temperature in K.
@@ -70,59 +87,60 @@ def solve(rho, T_c, Y_e, rho_r = -1, graphs = False, R_r = 6.9634e8, r_o = 0.000
         Whether to print messages about the result of integrations.
         When the function is to be used in a loop it's recommended to set to false.
         Default is True
-        
+
     Returns
     -------
-    
+
     core, reduced_core
-    
+
      core: class
         Contains values for the core in increasing radius according to
-        
+
         core.mass        | Mass in kg, array
         core.density     | Density in kg/m^3, array
         core.temperature | Temperature in K, array
         core.pressure    | Pressure in Pa, array
         core.radius      | Radius in m, array
-        
+
     reduced_core: class
         Same as core but values are reduced
     '''
 
     if X != -1 and Y != -1 and Z != -1:
-        mu = 2/(1+3*X+0.5*Y)
+        mu = 2 / (1 + 3 * X + 0.5 * Y)
     else:
         mu = 1
-    
+
     if rho_r == -1:
         rho_r = rho
-    
+
     rho_core_sun = 1.62e5
     rho_mean_sun = 1406.3134
-    R_sun = 6.9634*1e8
-    M_sun = 2*1e30
-    rho_e = rho*Y_e/sc.m_p
-    P_c = (3*sc.pi**2)**(2/3)*sc.hbar**2/(5*sc.m_e)*rho_e**(5/3)
-    rho_o = (sc.m_e*sc.c/sc.hbar)**(3)*sc.m_p/(3*sc.pi**2*Y_e)
-    q_o = rho_o/rho_r
-    
-    if x_max == -1:
-        x_max = 3e10/R_r
+    R_sun = 6.9634 * 1e8
+    M_sun = 2 * 1e30
+    rho_e = rho * Y_e / sc.m_p
+    P_c = (3 * sc.pi**2)**(2 / 3) * sc.hbar**2 / (5 * sc.m_e) * rho_e**(5 / 3)
+    rho_o = (sc.m_e * sc.c / sc.hbar)**(3) * sc.m_p / (3 * sc.pi**2 * Y_e)
+    q_o = rho_o / rho_r
 
-    def change_to_envelope(x,variables):
+    if x_max == -1:
+        x_max = 3e10 / R_r
+
+    def change_to_envelope(x, variables):
         q, M, t = variables
-        T = t*T_c
-        tf = (sc.hbar**2/(2*sc.m_e*sc.k)*(((3*sc.pi**2*rho_r*Y_e)/(sc.m_p))*q)**(2./3))
-        if T>=tf:
+        T = t * T_c
+        tf = (sc.hbar**2 / (2 * sc.m_e * sc.k) *
+              (((3 * sc.pi**2 * rho_r * Y_e) / (sc.m_p)) * q)**(2. / 3))
+        if T >= tf:
             envelope = 0
         else:
             envelope = 1
         return envelope
     change_to_envelope.terminal = True
 
-    def minimum_density(x,variables):
-        q, M, t = variables 
-        if q<=min_density:
+    def minimum_density(x, variables):
+        q, M, t = variables
+        if q <= min_density:
             terminate = 0
         elif np.isnan(q):
             terminate = 0
@@ -132,38 +150,53 @@ def solve(rho, T_c, Y_e, rho_r = -1, graphs = False, R_r = 6.9634e8, r_o = 0.000
     minimum_density.terminal = True
 
     def gamma(q):
-        y = q/q_o
-        gam = y**(2/3)/(3*(1+y**(2/3))**(1/2))
+        y = q / q_o
+        gam = y**(2 / 3) / (3 * (1 + y**(2 / 3))**(1 / 2))
         return gam
 
-    def core(x,variables):
-        q, M, t= variables
-        C = 4*sc.G*R_r**2*sc.m_p*sc.pi*rho_r/(3*Y_e*sc.c**2*sc.m_e)
-        derivatives = [-C*M*q/(gamma(q)*x**2),
-                      3*q*x**2,
-                      0]
-        return derivatives  
-    
+    def core(x, variables):
+        q, M, t = variables
+        C = 4 * sc.G * R_r**2 * sc.m_p * sc.pi * \
+            rho_r / (3 * Y_e * sc.c**2 * sc.m_e)
+        derivatives = [-C * M * q / (gamma(q) * x**2),
+                       3 * q * x**2,
+                       0]
+        return derivatives
+
     def pressure(q, P):
-        dpdq = Y_e*sc.c**2*gamma(q)*sc.m_e*rho_r/(P_c*sc.m_p)
+        dpdq = Y_e * sc.c**2 * gamma(q) * sc.m_e * rho_r / (P_c * sc.m_p)
         return dpdq
-        
+
     if density_event:
-        c_l = solve_ivp(core,[r_o,x_max],[rho/rho_r,0,1], events = [change_to_envelope,minimum_density], method = solver, rtol = r_tol, atol = a_tol)
+        c_l = solve_ivp(core,
+                        [r_o,
+                         x_max],
+                        [rho / rho_r,
+                         0,
+                         1],
+                        events=[change_to_envelope,
+                                minimum_density],
+                        method=solver,
+                        rtol=r_tol,
+                        atol=a_tol)
     else:
-        c_l = solve_ivp(core,[r_o,x_max],[rho/rho_r,0,1], events = change_to_envelope, method = solver, rtol = r_tol, atol = a_tol)
+        c_l = solve_ivp(
+            core, [
+                r_o, x_max], [
+                rho / rho_r, 0, 1], events=change_to_envelope, method=solver, rtol=r_tol, atol=a_tol)
 
     class core:
-        mass = c_l.y[1]*(4./3)*sc.pi*(R_r)**3*rho_r
+        mass = c_l.y[1] * (4. / 3) * sc.pi * (R_r)**3 * rho_r
         mass = mass[~np.isnan(mass)]
-        radius = c_l.t*R_r
+        radius = c_l.t * R_r
         radius = radius[0:len(mass)]
-        density = c_l.y[0]*rho_r
+        density = c_l.y[0] * rho_r
         density = density[~np.isnan(density)]
-        temperature = c_l.y[2]*T_c
+        temperature = c_l.y[2] * T_c
         temperature = temperature[0:len(mass)]
+
     class reduced_core:
-        
+
         mass = c_l.y[1]
         mass = mass[~np.isnan(mass)]
         radius = c_l.t
@@ -172,56 +205,85 @@ def solve(rho, T_c, Y_e, rho_r = -1, graphs = False, R_r = 6.9634e8, r_o = 0.000
         density = density[~np.isnan(density)]
         temperature = c_l.y[2]
         temperature = temperature[0:len(mass)]
-    
+
     if messages:
         print("Core:")
         print(c_l.message)
-        
-    p_f = (core.density[-1]/(m_u*mu*P_c))*sc.k*core.temperature[-1]
-    
-    pressure = solve_ivp(pressure,[reduced_core.density[-1],reduced_core.density[0]], [p_f], method = solver, t_eval = np.flip(reduced_core.density))
-    
+
+    p_f = (core.density[-1] / (m_u * mu * P_c)) * sc.k * core.temperature[-1]
+
+    pressure = solve_ivp(pressure,
+                         [reduced_core.density[-1],
+                          reduced_core.density[0]],
+                         [p_f],
+                         method=solver,
+                         t_eval=np.flip(reduced_core.density))
+
     if messages:
         print("Pressure:")
         print(pressure.message)
-    
-    core.pressure = pressure.y[0]*P_c
+
+    core.pressure = pressure.y[0] * P_c
     core.pressure = np.flip(core.pressure)
     reduced_core.pressure = pressure.y[0]
     reduced_core.pressure = np.flip(reduced_core.pressure)
-    
-    core.pressure_sm = ((2.*sc.pi*sc.hbar**2)/(5*sc.m_e))*(4*sc.pi/3)**(-2./3)*2**(-2./3)*(core.density*Y_e/sc.m_p)**(5./3)
-    reduced_core.pressure_sm = core.pressure/P_c
+
+    core.pressure_sm = ((2. * sc.pi * sc.hbar**2) / (5 * sc.m_e)) * (4 * sc.pi /
+                                                                     3)**(-2. / 3) * 2**(-2. / 3) * (core.density * Y_e / sc.m_p)**(5. / 3)
+    reduced_core.pressure_sm = core.pressure / P_c
 
     if graphs:
-        fig, ax = plt.subplots(2,2, figsize=(13,10), dpi=100)
-        ax[0,0].plot(core.radius/R_sun, core.mass/M_sun)
-        ax[0,0].set_xlabel('Core radius (R⊙)')
-        ax[0,0].set_ylabel('Core mass (M⊙)')
-        ax[0,0].set_title('Core mass (M⊙)')
-        ax[0,0].grid()
-        
-        ax[0,1].plot(core.radius/R_sun, core.density)
-        ax[0,1].set_xlabel('Core radius (R⊙)')
-        ax[0,1].set_ylabel('Core density (kg/m^3)')
-        ax[0,1].set_title('Core density (kg/m^2)')
-        ax[0,1].grid()
-        
-        ax[1,0].plot(core.radius/R_sun,core.pressure, label = 'dP/drho method')
+        fig, ax = plt.subplots(2, 2, figsize=(13, 10), dpi=100)
+        ax[0, 0].plot(core.radius / R_sun, core.mass / M_sun)
+        ax[0, 0].set_xlabel('Core radius (R⊙)')
+        ax[0, 0].set_ylabel('Core mass (M⊙)')
+        ax[0, 0].set_title('Core mass (M⊙)')
+        ax[0, 0].grid()
+
+        ax[0, 1].plot(core.radius / R_sun, core.density)
+        ax[0, 1].set_xlabel('Core radius (R⊙)')
+        ax[0, 1].set_ylabel('Core density (kg/m^3)')
+        ax[0, 1].set_title('Core density (kg/m^2)')
+        ax[0, 1].grid()
+
+        ax[1, 0].plot(core.radius / R_sun, core.pressure,
+                      label='dP/drho method')
         #ax[1,0].plot(core.radius/R_sun,((2.*sc.pi*sc.hbar**2)/(5*sc.m_e))*(4*sc.pi/3)**(-2./3)*2**(-2./3)*(core.density*Y_e/sc.m_p)**(5./3), label = 'Stat. Mec. Notes', linestyle = 'dotted', color ='orange')
-        ax[1,0].set_xlabel('Core radius (R⊙)')
-        ax[1,0].hlines((core.density[-1]/(m_u*mu))*sc.k*core.temperature[-1], core.radius[0]/R_sun, core.radius[-1]/R_sun, color = 'green', linestyle = '-.', label = 'PV=NkT')
-        ax[1,0].set_ylabel('Core pressure, logarithmic (Pa)')
-        ax[1,0].set_title(' Core pressure (Pa)')
-        ax[1,0].grid()
-        ax[1,0].legend()
-        ax[1,0].set_yscale('log')
-        
-        ax[1,1].plot(core.radius/R_sun, core.temperature, label = 'Core temperature', linestyle = 'dotted', color = 'orange')
-        ax[1,1].plot(core.radius/R_sun, (sc.hbar**2/(2*sc.m_e*sc.k)*(((3*sc.pi**2*rho_r*Y_e)/(sc.m_p))*reduced_core.density)**(2./3)), label = 'Fermi Temperature')
-        ax[1,1].set_ylabel('Temperature (K)')
-        ax[1,1].set_xlabel('Core radius (R⊙)')
-        ax[1,1].set_title('Core temperature (K)')
-        ax[1,1].grid()
-        ax[1,1].legend()
+        ax[1, 0].set_xlabel('Core radius (R⊙)')
+        ax[1,
+           0].hlines((core.density[-1] / (m_u * mu)) * sc.k * core.temperature[-1],
+                     core.radius[0] / R_sun,
+                     core.radius[-1] / R_sun,
+                     color='green',
+                     linestyle='-.',
+                     label='PV=NkT')
+        ax[1, 0].set_ylabel('Core pressure, logarithmic (Pa)')
+        ax[1, 0].set_title(' Core pressure (Pa)')
+        ax[1, 0].grid()
+        ax[1, 0].legend()
+        ax[1, 0].set_yscale('log')
+
+        ax[1,
+           1].plot(core.radius / R_sun,
+                   core.temperature,
+                   label='Core temperature',
+                   linestyle='dotted',
+                   color='orange')
+        ax[1, 1].plot(core.radius /
+                      R_sun, (sc.hbar**2 /
+                              (2 *
+                               sc.m_e *
+                               sc.k) *
+                              (((3 *
+                                 sc.pi**2 *
+                                 rho_r *
+                                 Y_e) /
+                                (sc.m_p)) *
+                                  reduced_core.density)**(2. /
+                                                          3)), label='Fermi Temperature')
+        ax[1, 1].set_ylabel('Temperature (K)')
+        ax[1, 1].set_xlabel('Core radius (R⊙)')
+        ax[1, 1].set_title('Core temperature (K)')
+        ax[1, 1].grid()
+        ax[1, 1].legend()
     return core, reduced_core
